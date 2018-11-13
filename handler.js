5b760fc3e9bd7f5c7ce9313b26ed85128801a27e
James Mullen <mail@james-mullen.me.uk>
2018-11-13 22:21:38 +0000

    Get recognition working from URL passed in from API Gateway request

--- handler.js
+++ handler.js
@@ -2,33 +2,79 @@
 
 const AWS = require('aws-sdk');
 const rek = new AWS.Rekognition();
+const fetch = require('node-fetch');
 
-function getAnimalFromRekogitionService() {
+const getData = async url => {
+  try {
+    const response = await fetch(url);
+    const data = await response.buffer();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+function getAnimalFromRekognitionService(imageData) {
+
      const params = {
       Image: {
-        S3Object: {
-          Bucket: "noservernovember-animal-bot-pics",
-          Name: "german.jpg",
-        },
+        Bytes: imageData
       },
       MaxLabels: 100,
       MinConfidence: 90,
     };
- 
+
     return new Promise((resolve, reject) => {
       rek.detectLabels(params, (err, data) => {
         if (err) {
           return reject(new Error(err));
         }
+
+	//need to check it is an animal
+	if (data.Labels.some( label => label.Name === 'Animal')) {
+
+	let maxSize = 0;
+	let result;
+
+	data.Labels.forEach((label,index) => {
+		if(label.Parents.length > maxSize ) {
+			maxSize = label.Parents.length;
+			result = label.Name;
+		}
+	});
+
+	console.log( maxSize + ' ' + result );
+
+	const matchingLabel = data.Labels.find( label => label.Name === result );
+
+        console.log(matchingLabel.Name + ' ' + matchingLabel.Parents);
+//	console.log('Result is ' + data.Labels[result].Name + ' ' + data.Labels[result].Parents.length);
+
+	var response = {
+        	statusCode: 200,
+        	body: JSON.stringify(matchingLabel.Name)
+    	};
         
         console.log(JSON.stringify(data));
-        //console.log('Analysis labels:', data.Labels);
-        return resolve(data.Labels);
+        return resolve(response);
+
+	} else {
+		var response = {
+                statusCode: 200,
+                body: JSON.stringify('Sorry that picture is not of an animal')
+		};
+		console.log(JSON.stringify(data));
+		return resolve(response);		
+	}
       });
     });
-}
+};
 
 
 exports.animalbot = async (event) => {
-  return getAnimalFromRekognitionService();
+  console.log(event);
+
+  const jsonBody = JSON.parse(event.body)
+  const imageData = await getData(jsonBody.url);
+  return getAnimalFromRekognitionService(imageData);
 };
