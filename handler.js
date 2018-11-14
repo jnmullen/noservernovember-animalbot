e1aace1ac632a2450405453eea7552cef1f029ba
James Mullen <james.mullen@bjss.com>
2018-11-14 09:13:11 +0000

    refactor get animal to be async/await rather than using a promise

--- handler.js
+++ handler.js
@@ -14,6 +14,55 @@ const getData = async url => {
     }
 };
 
+const getAnimalAsync = async imageData => {
+
+    const params = {
+        Image: {
+            Bytes: imageData
+        },
+        MaxLabels: 100,
+        MinConfidence: 90,
+    };
+
+    const data = await rek.detectLabels(params);
+
+    if (data.Labels.length > 0
+        && data.Labels.some(label => label.Name === 'Animal')) {
+
+        console.log('Found an animal - trying to now find the most specific match');
+
+        let maxSize = 0;
+        let result = '';
+
+        data.Labels.forEach((label) => {
+            if (label.Parents.length > maxSize) {
+                maxSize = label.Parents.length;
+                result = label.Name;
+            }
+        });
+
+        console.log('Most specific match is : ' + result);
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(`I can see ${matchingLabel.Name} in that image`)
+        };
+
+        return response;
+
+    } else {
+
+        console.log('Cannot find an animal in the image');
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify('Sorry that picture does not appear to contain an animal')
+        };
+
+        return response;
+    }
+};
+
 function getAnimalFromRekognitionService(imageData) {
 
     const params = {
@@ -62,6 +111,7 @@ function getAnimalFromRekognitionService(imageData) {
                     statusCode: 200,
                     body: JSON.stringify('Sorry that picture is not of an animal')
                 };
+
                 console.log(JSON.stringify(data));
                 return resolve(response);
             }
@@ -78,5 +128,5 @@ exports.animalbot = async (event) => {
 
     const imageData = await getData(jsonBody.url);
 
-    return getAnimalFromRekognitionService(imageData);
+    return getAnimalAsync(imageData);
 };
